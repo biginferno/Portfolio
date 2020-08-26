192222041d839836f65c24415e8b9136d9d5be61
biginferno <36423914+biginferno@users.noreply.github.com>
2020-08-26 10:45:23 -0700

    Updates

---
+++ precache-manifest.badd3cb530971e3d21d6043edd51fe6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923339788cc6d57e3a8b63b61660f7c5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "87afa04d528469f959bb",
+    "url": "/Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "0e9b61a6701492e53f1c",
+    "url": "/Portfolio/static/css/main.d7eb6c83.chunk.css"
+  },
+  {
+    "revision": "87afa04d528469f959bb",
+    "url": "/Portfolio/static/js/2.2b644dd1.chunk.js"
+  },
+  {
+    "revision": "677b11cbe3b89f475479895865fe263b",
+    "url": "/Portfolio/static/js/2.2b644dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9b61a6701492e53f1c",
+    "url": "/Portfolio/static/js/main.6f53303e.chunk.js"
+  },
+  {
+    "revision": "7aa19135429e5d6cf930",
+    "url": "/Portfolio/static/js/runtime-main.1bd230f8.js"
+  },
+  {
+    "revision": "cdc737b57026dc152e6ab4e742ccabbc",
+    "url": "/Portfolio/static/media/Profile.cdc737b5.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
